22cdd1383a8baca016e78fcd71dcc4c1a8559752
Andriy Plokhotnyuk <a.plokhotnyuk@thenewmotion.com>
2012-05-20 02:40:25 +0300

    removed redundant type parametrization

--- src/test/scala/com/github/plokhotnyuk/actors/Helper.scala
+++ src/test/scala/com/github/plokhotnyuk/actors/Helper.scala
@@ -3,19 +3,18 @@ package com.github.plokhotnyuk.actors
 object Helper {
   val availableProcessors = Runtime.getRuntime.availableProcessors()
 
-  def timed[T](name: String, n: Int)(benchmark: => T): T = {
+  def timed(name: String, n: Int)(benchmark: => Unit) {
     printf("\n%s:\n", name)
     val start = System.nanoTime
-    val result = benchmark
+    benchmark
     val duration = System.nanoTime - start
     printf("%,d ns\n", duration)
     printf("%,d ops\n", n)
     printf("%,d ns/op\n", duration / n)
     printf("%,d ops/s\n", (n * 1000000000L) / duration)
-    result
   }
 
-  def fork[T](code: => Unit): Thread = {
+  def fork(code: => Unit): Thread = {
     val thread = new Thread {
       override def run() {
         code
