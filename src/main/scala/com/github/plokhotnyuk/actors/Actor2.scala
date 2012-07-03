95f75345f1591c6a1d4fa88166170a29d4b010ed
Andriy Plokhotnyuk <a.plokhotnyuk@thenewmotion.com>
2012-07-03 11:07:37 +0300

    decreased latency of message handling at modified Scalaz's actor

--- src/main/scala/com/github/plokhotnyuk/actors/Actor2.scala
+++ src/main/scala/com/github/plokhotnyuk/actors/Actor2.scala
@@ -10,10 +10,10 @@ import java.util.concurrent.atomic.{AtomicInteger, AtomicReference}
  * Based on non-intrusive MPSC node-based queue, described by Dmitriy Vyukov:
  * http://www.1024cores.net/home/lock-free-algorithms/queues/non-intrusive-mpsc-node-based-queue
  */
-final case class Actor2[A](e: A => Unit, onError: Throwable => Unit = throw (_), batchSize: Int = 1024)(implicit s: Strategy)  {
+final case class Actor2[A](e: A => Unit, err: Throwable => Unit = throw (_), batch: Int = 1024)(implicit s: Strategy) {
   private[this] var anyA: A = _ // Don't know how to simplify this
-  private[this] val tail = new AtomicReference[Node[A]](new Node(anyA))
-  private[this] val head = new AtomicReference[Node[A]](tail.get)
+  @volatile private[this] var tail = new Node(anyA)
+  private[this] val head = new AtomicReference[Node[A]](tail)
   private[this] val suspended = new AtomicInteger(1)
 
   val toEffect: Effect[A] = effect[A](a => this ! a)
@@ -29,56 +29,47 @@ final case class Actor2[A](e: A => Unit, onError: Throwable => Unit = throw (_),
   }
 
   private[this] def trySchedule() {
-    if (suspended.compareAndSet(1, 0)) {
-      schedule()
-    }
+    if (suspended.compareAndSet(1, 0)) schedule()
   }
 
-  private[this] def schedule(): Any =
+  private[this] def schedule() {
     try {
-      act(())
+      s(act())
     } catch {
       case ex =>
         suspended.set(1)
         throw new RuntimeException(ex)
     }
+  }
 
-  private[this] val act: Effect[Unit] = effect {
-    (u: Unit) =>
-      val n = batchHandle(tail.get, batchSize)
-      tail.set(n) // synchronized action to see changes of non-volatile fields in any execution thread
-      if (n.get ne null) {
-        schedule()
-      } else {
-        suspended.set(1)
-        if (n.get ne null) {
-          trySchedule()
-        }
-      }
+  private[this] def act() {
+    val n = batchHandle(tail, batch)
+    if (n ne tail) {
+      tail = n
+      schedule()
+    } else {
+      suspended.set(1)
+      if (n.get ne null) trySchedule()
+    }
   }
 
   @tailrec
   private[this] def batchHandle(n: Node[A], i: Int): Node[A] = {
     val next = n.get
     if (next ne null) {
-      handle(next.a)
-      if (i > 0) {
-        batchHandle(next, i - 1)
-      } else next
+      try {
+        e(next.a)
+      } catch {
+        case ex => err(ex)
+      }
+      if (i > 0) batchHandle(next, i - 1) else next
     } else n
-  }
-
-  private[this] def handle(a: A) {
-    try {
-      e(a)
-    } catch {
-      case ex => onError(ex)
-    }
   }
 }
 
 trait Actors2 {
-  def actor2[A](e: A => Unit, err: Throwable => Unit = throw (_), batchSize: Int = 1024)(implicit s: Strategy): Actor2[A] = Actor2[A](e, err, batchSize)
+  def actor2[A](e: A => Unit, err: Throwable => Unit = throw (_), batch: Int = 1024)(implicit s: Strategy): Actor2[A] =
+    Actor2[A](e, err, batch)
 
   implicit def ActorFrom2[A](a: Actor2[A]): A => Unit = a ! _
 }
