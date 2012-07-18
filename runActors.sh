2163146e68d2a0d964ab4cb56d400a5827646176
Andriy Plokhotnyuk <a.plokhotnyuk@thenewmotion.com>
2012-07-18 18:30:15 +0300

    added script for batch running

---
+++ runActors.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+mvn -B clean install >out7-1.txt
+mvn -B clean install >out7-2.txt
+mvn -B clean install >out7-3.txt
+mvn -B clean install >out7-4.txt
+mvn -B clean install >out7-5.txt
