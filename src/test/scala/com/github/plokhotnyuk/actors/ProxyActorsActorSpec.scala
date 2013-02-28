1e6a42435e0a42df7634c6d23108409ea5b7b32a
Andriy Plokhotnyuk <a.plokhotnyuk@thenewmotion.com>
2013-02-28 17:40:15 +0200

    added benchmarks for ProxyActors + updated test results

---
+++ src/test/scala/com/github/plokhotnyuk/actors/ProxyActorsActorSpec.scala
@@ -0,0 +1,95 @@
+package com.github.plokhotnyuk.actors
+
+import api.actor._
+import java.util.concurrent.CountDownLatch
+
+class ProxyActorsActorSpec extends BenchmarkSpec {
+
+  "Single-producer sending" in {
+    val n = 10000000
+    val l = new CountDownLatch(1)
+    val a = tickActor(l, n)
+    timed(n) {
+      sendTicks(a, n)
+      l.await()
+    }
+    actorsFinished(a)
+  }
+
+  "Multi-producer sending" in {
+    val n = 10000000
+    val l = new CountDownLatch(1)
+    val a = tickActor(l, n)
+    timed(n) {
+      for (j <- 1 to CPUs) fork {
+        sendTicks(a, n / CPUs)
+      }
+      l.await()
+    }
+    actorsFinished(a)
+  }
+
+  "Max throughput" in {
+    val n = 20000000
+    val l = new CountDownLatch(CPUs)
+    val as = for (j <- 1 to CPUs) yield tickActor(l, n / CPUs)
+    timed(n) {
+      for (a <- as) fork {
+        sendTicks(a, n / CPUs)
+      }
+      l.await()
+    }
+    actorsFinished(as: _*)
+  }
+
+  "Ping between actors" in {
+    val n = 1000000
+    val l = new CountDownLatch(2)
+    val p1 = playerActor(l, n / 2)
+    val p2 = playerActor(l, n / 2)
+    timed(n) {
+      p1.ping(p2)
+      l.await()
+    }
+    actorsFinished(p1, p2)
+  }
+
+  private def tickActor(l: CountDownLatch, n: Int): TickActor = {
+    allCoresContext.proxyActor[TickActor](args = List((l, classOf[CountDownLatch]), (n, classOf[Int])))
+  }
+
+  private def sendTicks(a: TickActor, n: Int) {
+    var i = n
+    while (i > 0) {
+      a.countdown()
+      i -= 1
+    }
+  }
+
+  private def playerActor(l: CountDownLatch, n: Int): PlayerActor = {
+    allCoresContext.proxyActor[PlayerActor](args = List((l, classOf[CountDownLatch]), (n, classOf[Int])))
+  }
+}
+
+class TickActor(val l: CountDownLatch, val n: Int) {
+  private var i = n
+
+  def countdown() {
+    i -= 1
+    if (i == 0) {
+      l.countDown()
+    }
+  }
+}
+
+class PlayerActor(val l: CountDownLatch, val n: Int) {
+  private var i = n
+
+  def ping(p: PlayerActor) {
+    p.ping(this)
+    i -= 1
+    if (i == 0) {
+      l.countDown()
+    }
+  }
+}
