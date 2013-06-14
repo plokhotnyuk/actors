7c49b992a50824eda9a2db2e6f1d523d2fdd63d8
Andriy Plokhotnyuk <a.plokhotnyuk@thenewmotion.com>
2013-06-14 13:19:04 +0300

    removed (or marked as deprecated) operations that unsafe for concurrent modification of the queue

--- src/test/scala/com/github/plokhotnyuk/actors/ConcurrentLinkedBlockingQueue.scala
+++ src/test/scala/com/github/plokhotnyuk/actors/ConcurrentLinkedBlockingQueue.scala
@@ -4,9 +4,11 @@ import java.util
 import java.util.concurrent.{TimeUnit, Semaphore, BlockingQueue}
 import java.util.concurrent.atomic.AtomicReference
 import scala.annotation.tailrec
+import java.util.{ConcurrentModificationException, NoSuchElementException}
 
 /**
- * A high performance implementation of concurrent blocking queue.
+ * A partial implementation of concurrent blocking queue which is suitable for using in
+ * ThreadPoolExecutor and ExecutorCompletionService as high performance replacement of LinkedBlockingQueue.
  *
  * Implementation of task queue based on structure of non-intrusive MPSC node-based queue, described by Dmitriy Vyukov:
  * http://www.1024cores.net/home/lock-free-algorithms/queues/non-intrusive-mpsc-node-based-queue
@@ -16,14 +18,15 @@ import scala.annotation.tailrec
  */
 class ConcurrentLinkedBlockingQueue[A] extends util.AbstractQueue[A] with BlockingQueue[A] {
   private val head = new AtomicReference[Node[A]](new Node())
-  private val counter = new ReducibleSemaphore
+  private val count = new ReducibleSemaphore
   private val tail = new AtomicReference[Node[A]](head.get)
   private val none: A = null.asInstanceOf[A]
 
   def offer(e: A): Boolean = {
+    if (e == null) throw new NullPointerException
     val n = new Node(e)
     head.getAndSet(n).lazySet(n)
-    counter.release()
+    count.release()
     true
   }
 
@@ -33,11 +36,66 @@ class ConcurrentLinkedBlockingQueue[A] extends util.AbstractQueue[A] with Blocki
 
   def offer(e: A, timeout: Long, unit: TimeUnit): Boolean = offer(e)
 
-  def take(): A = poll()
+  def take(): A = {
+    count.acquire()
+    dequeue()
+  }
 
   def poll(): A = {
-    counter.acquire()
-    dequeue()
+    throw new UnsupportedOperationException("poll")
+  }
+
+  def poll(timeout: Long, unit: TimeUnit): A = if (count.tryAcquire(timeout, unit)) dequeue() else none
+
+  def remainingCapacity(): Int = Integer.MAX_VALUE - size()
+
+  @deprecated("can be safely used only for not concurrent modifications")
+  def drainTo(c: util.Collection[_ >: A]): Int = drainTo(c, Integer.MAX_VALUE)
+
+  @deprecated("can be safely used only for not concurrent modifications")
+  def drainTo(c: util.Collection[_ >: A], maxElements: Int): Int =
+    if (c eq null) throw new NullPointerException()
+    else if (c eq this) throw new IllegalArgumentException()
+    else if (maxElements <= 0) c.size()
+    else {
+      val tn = tail.get
+      val n = tn.get
+      if ((n ne null) && tail.compareAndSet(tn, n)) {
+        count.reducePermit()
+        c.add(n.a)
+        drainTo(c, maxElements - 1)
+      } else drainTo(c, maxElements)
+    }
+
+  def peek(): A = {
+    val n = peekNode()
+    if (n ne null) n.a else none
+  }
+
+  def iterator(): util.Iterator[A] = new util.Iterator[A] {
+    private var n = peekNode()
+
+    def hasNext: Boolean = n ne null
+
+    def next(): A = {
+      if (n eq null) throw new NoSuchElementException()
+      val a = n.a
+      n = n.get
+      a
+    }
+
+    def remove() {
+      throw new UnsupportedOperationException("remove")
+    }
+  }
+
+  def size(): Int = count.availablePermits()
+
+  @tailrec
+  private def peekNode(): Node[A] = {
+    val tn = tail.get
+    val n = tn.get
+    if ((n ne null) || (tn eq head.get)) n else peekNode() // Thank you √iktor! https://github.com/akka/akka/pull/1493/files
   }
 
   @tailrec
@@ -50,55 +108,6 @@ class ConcurrentLinkedBlockingQueue[A] extends util.AbstractQueue[A] with Blocki
       a
     } else dequeue()
   }
-
-  def poll(timeout: Long, unit: TimeUnit): A = {
-    if (counter.tryAcquire(timeout, unit)) poll() else none
-  }
-
-  def remainingCapacity(): Int = Integer.MAX_VALUE - size()
-
-  def drainTo(c: util.Collection[_ >: A]): Int = drainTo(c, Integer.MAX_VALUE)
-
-  def drainTo(c: util.Collection[_ >: A], maxElements: Int): Int =
-    if (maxElements <= 0) c.size()
-    else {
-      val tn = tail.get
-      val n = tn.get
-      if ((n ne null) && tail.compareAndSet(tn, n)) {
-        counter.reducePermit()
-        c.add(n.a)
-        drainTo(c, maxElements - 1)
-      } else drainTo(c, maxElements)
-    }
-
-  @tailrec
-  final def peek(): A = {
-    val tn = tail.get
-    val n = tn.get
-    if ((n ne null) || (tn eq head.get)) n.a else peek() // Thank you √iktor! https://github.com/akka/akka/pull/1493/files
-  }
-
-  def iterator(): util.Iterator[A] = new util.Iterator[A] {
-    private var tn = tail.get
-    private var n = tn.get
-
-    def hasNext: Boolean = (n ne null)
-
-    def next(): A = {
-      val a = n.a
-      tn = n
-      n = n.get
-      a
-    }
-
-    @tailrec
-    final def remove() {
-      if (tn.compareAndSet(n, n.get)) counter.reducePermit()
-      else remove()
-    }
-  }
-
-  def size(): Int = counter.availablePermits()
 }
 
 private class Node[A](var a: A = null.asInstanceOf[A]) extends AtomicReference[Node[A]]
