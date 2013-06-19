de5c87504031f97cffb55a38f6d8ebfdf6d372a8
Andriy Plokhotnyuk <a.plokhotnyuk@thenewmotion.com>
2013-06-19 14:52:08 +0300

    fixed property values in scripts

--- mvnAll.sh
+++ mvnAll.sh
@@ -2,5 +2,5 @@
 mvn -B clean test-compile >outX.txt
 mvn -B -Dbenchmark.executorServiceType=scala-forkjoin-pool test >>outX.txt
 mvn -B -Dbenchmark.executorServiceType=java-forkjoin-pool test >>outX.txt
-mvn -B -Dbenchmark.executorServiceType=fast-thread-pool test >>outX.txt
+mvn -B -Dbenchmark.executorServiceType=fixed-thread-pool test >>outX.txt
 mvn -B -Dbenchmark.executorServiceType=thread-pool test >>outX.txt
--- sbtAll.sh
+++ sbtAll.sh
@@ -2,5 +2,5 @@
 sbt clean test:compile >outX.txt
 sbt -Dbenchmark.executorServiceType=scala-forkjoin-pool test >>outX.txt
 sbt -Dbenchmark.executorServiceType=java-forkjoin-pool test >>outX.txt
-sbt -Dbenchmark.executorServiceType=fast-thread-pool test >>outX.txt
+sbt -Dbenchmark.executorServiceType=fixed-thread-pool test >>outX.txt
 sbt -Dbenchmark.executorServiceType=thread-pool test >>outX.txt
