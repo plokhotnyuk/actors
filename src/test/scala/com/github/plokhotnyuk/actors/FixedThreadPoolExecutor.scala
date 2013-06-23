0bde6cce5948b261c3e3e9db53161bd11eb05910
Andriy Plokhotnyuk <a.plokhotnyuk@thenewmotion.com>
2013-06-23 13:23:00 +0300

    clean up of docs

--- src/test/scala/com/github/plokhotnyuk/actors/FixedThreadPoolExecutor.scala
+++ src/test/scala/com/github/plokhotnyuk/actors/FixedThreadPoolExecutor.scala
@@ -14,8 +14,7 @@ import scala.annotation.tailrec
  * may be constructed with a given pool size; by default, equal to the number of available processors.
  *
  * <p>All threads are created in constructor call using a {@link ThreadFactory}.
- * If not otherwise specified, a default thread factory is used, that creates threads to all be
- * in the same {@link ThreadGroup} and with the same {@code NORM_PRIORITY} priority and daemon status.
+ * If not otherwise specified, a default thread factory is used, that creates threads with daemon status.
  *
  * <p>When running of tasks an uncaught exception can occurs. All unhandled exception are redirected to
  * provided handler that by default just print stack trace without stopping of worker thread execution.
@@ -54,19 +53,47 @@ class FixedThreadPoolExecutor(threadCount: Int = Runtime.getRuntime.availablePro
 
   threads.foreach(_.start())
 
+  /**
+   * Attempts to stop all actively executing tasks and discards of all previously submitted tasks,
+   * then blocks until all running tasks will be stopped.
+   *
+   * <p>This method does not wait for previously submitted tasks to
+   * complete execution.  Use {@link #awaitTermination awaitTermination}
+   * to do that.
+   */
   def shutdown() {
     shutdownNow()
     awaitTermination(0, TimeUnit.MILLISECONDS)
   }
 
+  /**
+   * Attempts to stop all actively executing tasks by interrupting of worker threads,
+   * and returns a list of the tasks that were submitted and awaiting for execution.
+   *
+   * <p>This method does not wait for actively executing tasks to terminate. Use {@link #awaitTermination} to do that.
+   *
+   * <p>Any task that fails to respond to interrupt (tight loop, etc.) may never terminate.
+   *
+   * @return list of tasks that never commenced execution
+   */
   def shutdownNow(): java.util.List[Runnable] = {
     running.lazySet(false)
     threads.filter(_ ne Thread.currentThread()).foreach(_.interrupt()) // don't interrupt worker thread due call in task
     drainTo(new java.util.LinkedList[Runnable](), tail.getAndSet(head.get)) // drain up to current head
   }
 
+  /**
+   * Returns <tt>true</tt> if shutdown of pool was started by {@link #shutdownNow} or {@link #shutdown} call.
+   *
+   * @return <tt>true</tt> if shutdown of pool was started.
+   */
   def isShutdown: Boolean = !running.get
 
+  /**
+   * Returns <tt>true</tt> if shutdown completed and all worker threads of pool are stopped.
+   *
+   * @return <tt>true</tt> if shutdown completed.
+   */
   def isTerminated: Boolean = terminations.getCount == 0
 
   def awaitTermination(timeout: Long, unit: TimeUnit): Boolean = {
@@ -76,7 +103,8 @@ class FixedThreadPoolExecutor(threadCount: Int = Runtime.getRuntime.availablePro
 
   /**
    * Executes the given task at some time in the future.
-   * Never throws {@link RejectedExecutionException} and all tasks which are submitted after {@link #shutdownNow} call
+   *
+   * <p>Never throws {@link RejectedExecutionException} and all tasks which are submitted after {@link #shutdownNow} call
    * are silently collected in the internal task queue that can be drained up by subsequent {@link #shutdownNow} call.
    *
    * @param task the runnable task
