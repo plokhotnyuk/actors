dd21aa610e31a39f03f914035b9d2f35bb4bb24c
Andriy Plokhotnyuk <a.plokhotnyuk@thenewmotion.com>
2013-06-28 16:54:51 +0300

    added name and threshold params

--- src/test/scala/com/github/plokhotnyuk/actors/FixedThreadPoolExecutor.scala
+++ src/test/scala/com/github/plokhotnyuk/actors/FixedThreadPoolExecutor.scala
@@ -1,8 +1,9 @@
 package com.github.plokhotnyuk.actors
 
 import java.util.concurrent._
-import java.util.concurrent.atomic.{AtomicBoolean, AtomicReference}
+import java.util.concurrent.atomic.{AtomicInteger, AtomicBoolean, AtomicReference}
 import java.util.concurrent.locks.LockSupport
+import scala.annotation.tailrec
 
 /**
  * A high performance implementation of an `java.util.concurrent.ExecutorService ExecutorService`
@@ -28,12 +29,15 @@ import java.util.concurrent.locks.LockSupport
  *
  * Cooked at kitchen of <a href="https://github.com/plokhotnyuk/actors">actor benchmarks</a>.
  *
- * @param threadCount   A number of worker threads in pool
- * @param threadFactory A factory to be used to build worker threads
- * @param onError       The exception handler for unhandled errors during executing of tasks.
- * @param onReject      The handler for rejection of task submission after shutdown
+ * @param threadCount       A number of worker threads in pool
+ * @param threadFactory     A factory to be used to build worker threads
+ * @param onError           The exception handler for unhandled errors during executing of tasks.
+ * @param onReject          The handler for rejection of task submission after shutdown
+ * @param slowdownThreshold A number of tries before slowdown of worker thread when task queue is empty
+ * @param parkThreshold     A number of tries before parking of worker thread when task queue is empty
+ * @param name              A name of the executor service
  */
-class FixedThreadPoolExecutor(threadCount: Int = FixedThreadPoolExecutor.processors,
+class FixedThreadPoolExecutor(threadCount: Int = Runtime.getRuntime.availableProcessors(),
                               threadFactory: ThreadFactory = new ThreadFactory() {
                                 def newThread(worker: Runnable): Thread = new Thread(worker) {
                                   setDaemon(true)
@@ -42,21 +46,32 @@ class FixedThreadPoolExecutor(threadCount: Int = FixedThreadPoolExecutor.process
                               onError: Throwable => Unit = _.printStackTrace(),
                               onReject: Runnable => Unit = {
                                 t => throw new RejectedExecutionException("Task " + t + " rejected.")
+                              },
+                              slowdownThreshold: Int = 700,
+                              parkThreshold: Int = 1000,
+                              name: String = {
+                                "FixedThreadPool-" + FixedThreadPoolExecutor.poolId.getAndAdd(1)
                               }) extends AbstractExecutorService {
   private val head = new AtomicReference[TaskNode](new TaskNode())
   private val running = new AtomicBoolean(true)
-  private val lastWaiter = new AtomicReference[Thread]()
+  private val waiters = new AtomicReference[List[Thread]](Nil)
   private val terminations = new CountDownLatch(threadCount)
   private val tail = new AtomicReference[TaskNode](head.get)
   private val threads = {
-    val tf = threadFactory // to avoid creating of field for the threadFactory constructor param
-    val t = tail
+    val t = tail // to avoid long field names
     val r = running
-    val oe = onError
     val ts = terminations
-    val lw = lastWaiter
-    val st = FixedThreadPoolExecutor.processors * 125 / threadCount.toString.length
-    (1 to threadCount).map(_ => tf.newThread(new Worker(t, r, oe, ts, lw, st)))
+    val ws = waiters
+    val tf = threadFactory // to avoid creating of field for the threadFactory constructor param
+    val oe = onError
+    val st = slowdownThreshold
+    val pt = parkThreshold
+    (1 to threadCount).map {
+      i =>
+        val wt = tf.newThread(new Worker(t, r, oe, st, pt, ts, ws))
+        wt.setName(name + "-worker-" + i)
+        wt
+    }
   }
 
   threads.foreach(t => t.getState match {
@@ -88,14 +103,24 @@ class FixedThreadPoolExecutor(threadCount: Int = FixedThreadPoolExecutor.process
   def execute(task: Runnable) {
     if (running.get) {
       enqueue(task)
-      LockSupport.unpark(lastWaiter.get)
+      unpark()
     } else handleReject(task)
   }
+
+  override def toString: String = name
 
   private def enqueue(task: Runnable) {
     if (task eq null) throw new NullPointerException
     val n = new TaskNode(task)
     head.getAndSet(n).lazySet(n)
+  }
+
+  @tailrec
+  private def unpark() {
+    val ws = waiters.get
+    if (ws.isEmpty) ()
+    else if (waiters.compareAndSet(ws, ws.tail)) LockSupport.unpark(ws.head)
+    else unpark()
   }
 
   @annotation.tailrec
@@ -120,13 +145,14 @@ class FixedThreadPoolExecutor(threadCount: Int = FixedThreadPoolExecutor.process
   }
 }
 
-private class Worker(tail: AtomicReference[TaskNode], running: AtomicBoolean, onError: Throwable => Unit,
-                     terminations: CountDownLatch, lastWaiter: AtomicReference[Thread], spinThreshold: Int) extends Runnable {
+private class Worker(tail: AtomicReference[TaskNode], running: AtomicBoolean,
+                     onError: Throwable => Unit, slowdownThreshold: Int, parkThreshold: Int,
+                     terminations: CountDownLatch, waiters: AtomicReference[List[Thread]]) extends Runnable {
   var backOffs = 0
 
   def run() {
     try {
-      while (running.get || isNotEmpty) {
+      while (running.get || !isEmpty) {
         try {
           dequeueAndRun()
         } catch {
@@ -142,32 +168,38 @@ private class Worker(tail: AtomicReference[TaskNode], running: AtomicBoolean, on
   private def dequeueAndRun() {
     val tn = tail.get
     val n = tn.get
-    if ((n ne null) && tail.compareAndSet(tn, n)) {
-      val t = n.task
-      n.task = null
-      t.run()
-      backOffs = 0
-    } else {
+    if (n eq null) {
       backOff()
       dequeueAndRun()
-    }
+    } else if (tail.compareAndSet(tn, n)) {
+      n.task.run()
+      n.task = null
+      tuneBackOff()
+    } else dequeueAndRun()
   }
 
   private def backOff() {
     backOffs += 1
-    if (backOffs < spinThreshold) ()
-    else if (backOffs < 1000) LockSupport.parkNanos(1)
-    else {
-      val ct = Thread.currentThread()
-      val lw = lastWaiter.getAndSet(ct)
-      LockSupport.park()
-      lastWaiter.set(lw)
-      backOffs = 0
-      if (Thread.interrupted()) ct.interrupt()
-    }
+    if (backOffs < slowdownThreshold) ()
+    else if (backOffs < parkThreshold) LockSupport.parkNanos(1)
+    else park(Thread.currentThread())
   }
 
-  private def isNotEmpty: Boolean = (tail.get.get ne null) // used only after shutdown call: no new tasks expected
+  @tailrec
+  private def park(w: Thread) {
+    val ws = waiters.get
+    if (waiters.compareAndSet(ws, w :: ws)) {
+      LockSupport.park()
+      if (Thread.interrupted()) w.interrupt()
+      backOffs = 0
+    } else park(w)
+  }
+
+  private def tuneBackOff() {
+    backOffs = Math.max(slowdownThreshold - backOffs, 0)
+  }
+
+  private def isEmpty: Boolean = tail.get.get eq null
 
   private def handleError(ex: Throwable) {
     if (running.get || !ex.isInstanceOf[InterruptedException]) onError(ex)
@@ -175,7 +207,7 @@ private class Worker(tail: AtomicReference[TaskNode], running: AtomicBoolean, on
 }
 
 private object FixedThreadPoolExecutor {
-  private val processors = Runtime.getRuntime.availableProcessors()
+  private val poolId = new AtomicInteger(1)
   private val shutdownPerm = new RuntimePermission("modifyThread")
 }
 
