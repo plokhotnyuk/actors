4fd7a8c9613f554dc8bbe744c7a8acefcf401313
Andriy Plokhotnyuk <a.plokhotnyuk@thenewmotion.com>
2013-07-11 18:06:21 +0300

    try to run tests for all executors on Travis CI

--- travis-ci.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-mvn -B test-compile
-mvn -B -Dbenchmark.executorServiceType=fixed-thread-pool test -Ptravis
-mvn -B -Dbenchmark.executorServiceType=scala-forkjoin-pool test -Ptravis
-mvn -B -Dbenchmark.executorServiceType=java-forkjoin-pool test -Ptravis
-mvn -B -Dbenchmark.executorServiceType=thread-pool test -Ptravis
