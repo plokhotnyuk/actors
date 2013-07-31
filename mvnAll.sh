cfaaf0341c62390bae81387cf5a2137a49cf7745
Andriy Plokhotnyuk <a.plokhotnyuk@thenewmotion.com>
2013-07-31 07:22:43 +0300

    set parallelism = 100 to get better max throughput + updated results from notebook & desktop

--- mvnAll.sh
+++ mvnAll.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 mvn -B clean test-compile >outX.txt
-mvn -B -Dbenchmark.executorServiceType=fixed-thread-pool test >>outX.txt
-mvn -B -Dbenchmark.executorServiceType=scala-forkjoin-pool test >>outX.txt
-mvn -B -Dbenchmark.executorServiceType=java-forkjoin-pool test >>outX.txt
-mvn -B -Dbenchmark.executorServiceType=thread-pool test >>outX.txt
+mvn -B -Dbenchmark.parallelism=100 -Dbenchmark.executorServiceType=fixed-thread-pool test >>outX.txt
+mvn -B -Dbenchmark.parallelism=100 -Dbenchmark.executorServiceType=scala-forkjoin-pool test >>outX.txt
+mvn -B -Dbenchmark.parallelism=100 -Dbenchmark.executorServiceType=java-forkjoin-pool test >>outX.txt
+mvn -B -Dbenchmark.parallelism=100 -Dbenchmark.executorServiceType=thread-pool test >>outX.txt
