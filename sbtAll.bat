2f910f894d1e9dc5a539873628d45c135ee29511
Andriy Plokhotnyuk <a.plokhotnyuk@thenewmotion.com>
2013-11-04 16:54:07 +0200

    back to optimal value of parallelism for "Multi-producer sending" and "Max throughput" benchmarks

--- sbtAll.bat
+++ sbtAll.bat
@@ -1,6 +1,6 @@
 @echo off
 call sbt clean test:compile exit >outX.txt
-call sbt -Dbenchmark.parallelism=100 -Dbenchmark.executorServiceType=fixed-thread-pool test >>outX.txt
-call sbt -Dbenchmark.parallelism=100 -Dbenchmark.executorServiceType=scala-forkjoin-pool test >>outX.txt
-call sbt -Dbenchmark.parallelism=100 -Dbenchmark.executorServiceType=java-forkjoin-pool test >>outX.txt
-call sbt -Dbenchmark.parallelism=100 -Dbenchmark.executorServiceType=thread-pool test >>outX.txt
+call sbt -Dbenchmark.executorServiceType=fixed-thread-pool test >>outX.txt
+call sbt -Dbenchmark.executorServiceType=scala-forkjoin-pool test >>outX.txt
+call sbt -Dbenchmark.executorServiceType=java-forkjoin-pool test >>outX.txt
+call sbt -Dbenchmark.executorServiceType=thread-pool test >>outX.txt
