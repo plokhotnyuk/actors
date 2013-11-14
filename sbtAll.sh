3aa18c04b4c0db710396ba8d76793f0d453bec13
Andriy Plokhotnyuk <a.plokhotnyuk@thenewmotion.com>
2013-11-14 21:08:05 +0200

    back to default value of parallelism option

--- sbtAll.sh
+++ sbtAll.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sbt clean test:compile >outX.txt
-sbt -Dbenchmark.parallelism=100 -Dbenchmark.executorServiceType=fixed-thread-pool test >>outX.txt
-sbt -Dbenchmark.parallelism=100 -Dbenchmark.executorServiceType=scala-forkjoin-pool test >>outX.txt
-sbt -Dbenchmark.parallelism=100 -Dbenchmark.executorServiceType=java-forkjoin-pool test >>outX.txt
-sbt -Dbenchmark.parallelism=100 -Dbenchmark.executorServiceType=thread-pool test >>outX.txt
+sbt -Dbenchmark.executorServiceType=fixed-thread-pool test >>outX.txt
+sbt -Dbenchmark.executorServiceType=scala-forkjoin-pool test >>outX.txt
+sbt -Dbenchmark.executorServiceType=java-forkjoin-pool test >>outX.txt
+sbt -Dbenchmark.executorServiceType=thread-pool test >>outX.txt
