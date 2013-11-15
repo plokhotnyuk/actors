bd16dbe48cd62c474b8dc825c98041e7b9845900
Andriy Plokhotnyuk <a.plokhotnyuk@thenewmotion.com>
2013-11-15 12:36:38 +0200

    revised implementation FixedThreadPoolExecutor to used fast MSMP queue with sync instead of ConcurrentLinkedQueue with semaphore + updated result of benchmark from laptop

--- src/test/scala/com/github/plokhotnyuk/actors/FixedThreadPoolExecutor.scala
+++ src/test/scala/com/github/plokhotnyuk/actors/FixedThreadPoolExecutor.scala
@@ -2,7 +2,8 @@ package com.github.plokhotnyuk.actors
 
 import java.util
 import java.util.concurrent._
-import java.util.concurrent.atomic.AtomicInteger
+import java.util.concurrent.atomic.{AtomicReference, AtomicInteger}
+import java.util.concurrent.locks.AbstractQueuedSynchronizer
 
 /**
  * An implementation of an `java.util.concurrent.ExecutorService ExecutorService`
@@ -23,30 +24,41 @@ import java.util.concurrent.atomic.AtomicInteger
  * `java.util.concurrent.RejectedExecutionException` can occurs only after shutdown
  * when pool was initialized with default implementation of `onReject: Runnable => Unit`.
  *
+ * Implementation based on non-intrusive MPSC node-based queue, modified for MPMC access:
+ * [[http://www.1024cores.net/home/lock-free-algorithms/queues/non-intrusive-mpsc-node-based-queue]]
+ *
+ * Idea of using some implementation of 'java.util.concurrent.locks.AbstractQueuedSynchronizer' from
+ * [[https://github.com/laforge49/JActor2/blob/master/jactor2-core/src/main/java/org/agilewiki/jactor2/core/facilities/ThreadManager.java]]
+ *
  * @param poolSize       A number of worker threads in pool
  * @param threadFactory  A factory to be used to build worker threads
  * @param onError        The exception handler for unhandled errors during executing of tasks
  * @param onReject       The handler for rejection of task submission after shutdown
  * @param name           A name of the executor service
  */
-class FixedThreadPoolExecutor(poolSize: Int = Runtime.getRuntime.availableProcessors(),
+class FixedThreadPoolExecutor(poolSize: Int = Runtime.getRuntime.availableProcessors,
                               threadFactory: ThreadFactory = FixedThreadPoolExecutor.daemonThreadFactory(),
                               onError: Throwable => Unit = _.printStackTrace(),
                               onReject: Runnable => Unit = t => throw new RejectedExecutionException(t.toString),
                               name: String = FixedThreadPoolExecutor.generateName()) extends AbstractExecutorService {
-  private val tasks = new ConcurrentLinkedQueue[Runnable]()
-  private val taskCount = new Semaphore(0)
-  private val state = new AtomicInteger() // pool state (0 - running, 1 - shutdown, 2 - stop)
+  private val sync = new AbstractQueuedSynchronizer {
+    override protected final def tryReleaseShared(ignore: Int): Boolean = true
+
+    override protected final def tryAcquireShared(ignore: Int): Int = work()
+  }
+  private val tail = new AtomicReference[TaskNode](new TaskNode)
+  private val state = new AtomicInteger // pool state (0 - running, 1 - shutdown, 2 - stop)
+  private val head = new AtomicReference[TaskNode](tail.get)
   private val terminations = new CountDownLatch(poolSize)
   private val threads = {
     val tf = threadFactory // to avoid creating of fields for a constructor params
     val ts = terminations // to avoid long field name
     (1 to poolSize).map {
       i =>
-        val wt = tf.newThread(new Runnable() {
+        val wt = tf.newThread(new Runnable {
           def run(): Unit =
-            try doWork(state.get) catch {
-              case ex: InterruptedException => // can occurs on shutdownNow when worker is backing off
+            try loop() catch {
+              case _: InterruptedException => // ignore
             } finally ts.countDown()
         })
         wt.setName(name + "-worker-" + i)
@@ -63,8 +75,8 @@ class FixedThreadPoolExecutor(poolSize: Int = Runtime.getRuntime.availableProces
   def shutdownNow(): util.List[Runnable] = {
     checkShutdownAccess()
     setState(2)
-    threads.filter(_ ne Thread.currentThread()).foreach(_.interrupt()) // don't interrupt worker thread due call in task
-    drainTo(new util.LinkedList[Runnable]())
+    threads.filter(_ ne Thread.currentThread).foreach(_.interrupt()) // don't interrupt worker thread due call in task
+    drainTo(new util.LinkedList[Runnable])
   }
 
   def isShutdown: Boolean = state.get != 0
@@ -72,43 +84,61 @@ class FixedThreadPoolExecutor(poolSize: Int = Runtime.getRuntime.availableProces
   def isTerminated: Boolean = terminations.getCount == 0
 
   def awaitTermination(timeout: Long, unit: TimeUnit): Boolean = {
-    taskCount.release(threads.size)
-    if (threads.exists(_ eq Thread.currentThread())) terminations.countDown() // don't hang up due call in task
+    if (threads.exists(_ eq Thread.currentThread)) terminations.countDown() // don't hang up due call in task
     terminations.await(timeout, unit)
   }
 
   def execute(t: Runnable): Unit =
-    if (t eq null) throw new NullPointerException()
+    if (t eq null) throw new NullPointerException
     else if (state.get != 0) onReject(t)
     else {
-      tasks.offer(t)
-      taskCount.release()
+      val n = new TaskNode(t)
+      head.getAndSet(n).set(n)
+      sync.releaseShared(1)
     }
 
-  override def toString: String =
-    super.toString + "[" + status + ", pool size = " + threads.size + ", name = " + name + "]"
+  override def toString: String = s"${super.toString}[$status], pool size = ${threads.size}, name = $name]"
 
   @annotation.tailrec
   private def drainTo(ts: util.List[Runnable]): util.List[Runnable] = {
-    val t = tasks.poll()
-    if (t eq null) ts
-    else {
-      ts.add(t)
+    val tn = tail.get
+    val n = tn.get
+    if (n eq null) ts
+    else if (tail.compareAndSet(tn, n)) {
+      ts.add(n.task)
+      n.task = null
       drainTo(ts)
-    }
+    } else drainTo(ts)
   }
 
   @annotation.tailrec
-  private def doWork(s: Int): Unit = {
-    taskCount.acquire()
-    run(tasks.poll())
-    if (s != 2) doWork(state.get)
+  private def loop(): Unit = {
+    sync.acquireSharedInterruptibly(1)
+    loop()
   }
 
-  private def run(t: Runnable): Unit =
-    if (t ne null) try t.run() catch {
-      case ex: Throwable => if (!ex.isInstanceOf[InterruptedException] || state.get != 2) onError(ex)
+  private def work(): Int = {
+    val tn = tail.get
+    val n = tn.get
+    if (n eq null) sleepOrStop()
+    else {
+      if (tail.compareAndSet(tn, n)) run(n)
+      workOrStop()
     }
+  }
+
+  private def workOrStop(): Int =
+    if (state.get != 2) 1
+    else throw new InterruptedException
+
+  private def sleepOrStop(): Int =
+    if (state.get == 0) -1
+    else throw new InterruptedException
+
+  private def run(n: TaskNode): Unit =
+    try n.task.run() catch {
+      case ex: Throwable => if (!ex.isInstanceOf[InterruptedException] || state.get != 2) onError(ex)
+    } finally n.task = null
 
   @annotation.tailrec
   private def setState(newState: Int): Unit = {
@@ -133,14 +163,16 @@ class FixedThreadPoolExecutor(poolSize: Int = Runtime.getRuntime.availableProces
 }
 
 private object FixedThreadPoolExecutor {
-  private val poolId = new AtomicInteger()
+  private val poolId = new AtomicInteger
   private val shutdownPerm = new RuntimePermission("modifyThread")
-  
-  def daemonThreadFactory(): ThreadFactory = new ThreadFactory() {
+
+  def daemonThreadFactory(): ThreadFactory = new ThreadFactory {
     def newThread(worker: Runnable): Thread = new Thread(worker) {
       setDaemon(true)
     }
   }
-  
+
   def generateName(): String = "FixedThreadPool-" + poolId.incrementAndGet()
 }
+
+private class TaskNode(var task: Runnable = null) extends AtomicReference[TaskNode]
