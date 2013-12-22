cb9e318d299fc754199c1f5b7151177b2cfb89bc
Andriy Plokhotnyuk <a.plokhotnyuk@thenewmotion.com>
2013-12-22 07:49:09 +0200

    fix of possible memory leak + minimize batch loop

--- src/test/scala/com/github/plokhotnyuk/actors/Actor2.scala
+++ src/test/scala/com/github/plokhotnyuk/actors/Actor2.scala
@@ -39,9 +39,10 @@ final case class Actor2[A](handler: A => Unit, onError: Throwable => Unit = thro
 
   private def act(t: Node[A]): Unit = {
     val n = batchHandle(t, 1024)
+    tail = n
+    n.a = null.asInstanceOf[A]
     if ((n ne t) && (n.get ne null)) strategy(act(n))
     else {
-      tail = n
       state.set(0)
       if ((n.get ne null) && state.compareAndSet(0, 1)) strategy(act(n))
     }
@@ -53,7 +54,7 @@ final case class Actor2[A](handler: A => Unit, onError: Throwable => Unit = thro
     if ((n ne null) && i > 0) {
       try handler(n.a) catch {
         case ex: Throwable => onError(ex)
-      } finally n.a = null.asInstanceOf[A]
+      }
       batchHandle(n, i - 1)
     } else t
   }
