0cd40f9955eccc84099af182ec10110fa663dc5d
Andriy Plokhotnyuk <a.plokhotnyuk@thenewmotion.com>
2014-02-16 15:36:24 +0200

    add sleeping during GC to speed up benchmarks on Travis CI build

--- src/test/scala/com/github/plokhotnyuk/actors/BenchmarkSpec.scala
+++ src/test/scala/com/github/plokhotnyuk/actors/BenchmarkSpec.scala
@@ -104,7 +104,7 @@ object BenchmarkSpec {
     @annotation.tailrec
     def forceGC(prevUsage: Long = usage): Long = {
       System.gc()
-      Thread.sleep(30)
+      Thread.sleep(50)
       val currUsage = usage
       if (currUsage >= prevUsage) forceGC(prevUsage)
       else currUsage
@@ -113,13 +113,13 @@ object BenchmarkSpec {
     @annotation.tailrec
     def fullGC(precision: Double, prevUsage: Long = forceGC()): Long = {
       System.gc()
-      Thread.sleep(30)
+      Thread.sleep(50)
       val currUsage = usage
       if (Math.abs(prevUsage - currUsage).toDouble / prevUsage > precision) fullGC(precision, currUsage)
       else currUsage
     }
 
-    fullGC(0.003)
+    fullGC(0.001)
   }
 
   def fullShutdown(e: ExecutorService): Unit = {
