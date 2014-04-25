ec53ba67ec9229c994e26ff979f30846fba541d7
Andriy Plokhotnyuk <a.plokhotnyuk@thenewmotion.com>
2014-04-26 00:22:37 +0300

    tuning + code clean up for NonBlockingBoundedMailbox

--- src/test/scala/com/github/plokhotnyuk/actors/NonBlockingBoundedMailbox.scala
+++ src/test/scala/com/github/plokhotnyuk/actors/NonBlockingBoundedMailbox.scala
@@ -1,56 +1,67 @@
 package com.github.plokhotnyuk.actors
 
-import akka.dispatch._
-import akka.actor.{ActorSystem, ActorRef}
-import com.typesafe.config.Config
 import java.util.concurrent.atomic.AtomicReference
+import scala.util.control.NoStackTrace
+import com.typesafe.config.Config
+import akka.actor.{ActorSystem, ActorRef}
+import akka.AkkaException
+import akka.dispatch._
 
-class NonBlockingBoundedMailbox(val bound: Int) extends MailboxType with ProducesMessageQueue[MessageQueue] {
-  if (bound <= 0) throw new IllegalArgumentException
+class NonBlockingBoundedMailbox(bound: Int = Int.MaxValue) extends MailboxType with ProducesMessageQueue[MessageQueue] {
+  if (bound <= 0) throw new IllegalArgumentException("Mailbox bound should be greater than 0")
 
   def this(settings: ActorSystem.Settings, config: Config) = this(config.getInt("mailbox-bound"))
 
   override def create(owner: Option[ActorRef], system: Option[ActorSystem]): MessageQueue = new NBBQ(bound)
 }
 
-private class NBBQ(bound: Int) extends AtomicReference(new NBBQNode) with MessageQueue {
+@SerialVersionUID(1L)
+case class OutOfMailboxBoundsException(message: String) extends AkkaException(message) with NoStackTrace
+
+private final class NBBQ(bound: Int) extends AtomicReference(new NBBQNode) with MessageQueue {
   private var count = 0
   private val tail = new AtomicReference(get)
 
   override def enqueue(receiver: ActorRef, handle: Envelope): Unit = offer(new NBBQNode(handle))
 
-  override def dequeue(): Envelope = poll()
+  override def dequeue(): Envelope = poll(tail)
 
   override def numberOfMessages: Int = get.count - count
 
-  override def hasMessages: Boolean = numberOfMessages > 0
+  override def hasMessages: Boolean = get.count != count
 
-  override def cleanUp(owner: ActorRef, deadLetters: MessageQueue): Unit =
-    while (hasMessages) {
-      deadLetters.enqueue(owner, dequeue())
+  @annotation.tailrec
+  override def cleanUp(owner: ActorRef, deadLetters: MessageQueue): Unit = {
+    val env = dequeue()
+    if (env ne null) {
+      deadLetters.enqueue(owner, env)
+      cleanUp(owner, deadLetters)
     }
+  }
 
   @annotation.tailrec
   private def offer(n: NBBQNode): Unit = {
     val h = get
     val hc = h.count
-    if (hc - count >= bound) throw new OutOfMessageQueueBoundsException
-    n.count = hc + 1
-    if (compareAndSet(h, n)) h.lazySet(n)
-    else offer(n)
+    if (hc - count >= bound) throw new OutOfMailboxBoundsException("Mailbox boundary exceeded")
+    else {
+      n.count = hc + 1
+      if (compareAndSet(h, n)) h.lazySet(n)
+      else offer(n)
+    }
   }
 
   @annotation.tailrec
-  private def poll(): Envelope = {
-    val t = tail.get
-    val n = t.get
+  private def poll(t: AtomicReference[NBBQNode]): Envelope = {
+    val tn = t.get
+    val n = tn.get
     if (n ne null) {
       count = n.count
-      if (tail.compareAndSet(t, n)) {
+      if (t.compareAndSet(tn, n)) {
         val e = n.env
         n.env = null // to avoid possible memory leak when queue is empty
         e
-      } else poll()
+      } else poll(t)
     } else null
   }
 }
