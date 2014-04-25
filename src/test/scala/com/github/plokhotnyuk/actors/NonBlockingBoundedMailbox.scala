dc1b81727ac336f9807b91f116ad4c5130a630a1
Andriy Plokhotnyuk <a.plokhotnyuk@thenewmotion.com>
2014-04-26 02:26:44 +0300

    try to get rid off count field

--- src/test/scala/com/github/plokhotnyuk/actors/NonBlockingBoundedMailbox.scala
+++ src/test/scala/com/github/plokhotnyuk/actors/NonBlockingBoundedMailbox.scala
@@ -19,16 +19,15 @@ class NonBlockingBoundedMailbox(bound: Int = Int.MaxValue) extends MailboxType w
 case class OutOfMailboxBoundsException(message: String) extends AkkaException(message) with NoStackTrace
 
 private final class NBBQ(bound: Int) extends AtomicReference(new NBBQNode) with MessageQueue {
-  private var count = 0
   private val tail = new AtomicReference(get)
 
   override def enqueue(receiver: ActorRef, handle: Envelope): Unit = offer(new NBBQNode(handle))
 
   override def dequeue(): Envelope = poll(tail)
 
-  override def numberOfMessages: Int = get.count - count
+  override def numberOfMessages: Int = get.count - tail.get.count
 
-  override def hasMessages: Boolean = get.count != count
+  override def hasMessages: Boolean = get ne tail.get
 
   @annotation.tailrec
   override def cleanUp(owner: ActorRef, deadLetters: MessageQueue): Unit = {
@@ -43,11 +42,11 @@ private final class NBBQ(bound: Int) extends AtomicReference(new NBBQNode) with 
   private def offer(n: NBBQNode): Unit = {
     val h = get
     val hc = h.count
-    if (hc - count < bound) {
+    if (hc - tail.get.count < bound) {
       n.count = hc + 1
       if (compareAndSet(h, n)) h.lazySet(n)
       else offer(n)
-    } else throw new OutOfMailboxBoundsException("Mailbox boundary exceeded")
+    } else throw new OutOfMailboxBoundsException("Mailbox bound exceeded")
   }
 
   @annotation.tailrec
@@ -55,7 +54,6 @@ private final class NBBQ(bound: Int) extends AtomicReference(new NBBQNode) with 
     val tn = t.get
     val n = tn.get
     if (n ne null) {
-      count = n.count
       if (t.compareAndSet(tn, n)) {
         val e = n.env
         n.env = null // to avoid possible memory leak when queue is empty
