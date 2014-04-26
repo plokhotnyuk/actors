ecea5bec4c911bf2f83f88f0437a0ce85802c12b
Andriy Plokhotnyuk <a.plokhotnyuk@thenewmotion.com>
2014-04-26 08:43:53 +0300

    added scripts for launching benchmark with different size of thread pools for actors

---
+++ mvnAll_poolSize10.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+mvn -B clean test-compile >outX_poolSize10.txt
+mvn -B -Dbenchmark.poolSize=10 -Dbenchmark.executorServiceType=scala-forkjoin-pool test >>outX_poolSize10.txt
+mvn -B -Dbenchmark.poolSize=10 -Dbenchmark.executorServiceType=java-forkjoin-pool test >>outX_poolSize10.txt
+mvn -B -Dbenchmark.poolSize=10 -Dbenchmark.executorServiceType=jsr166e-forkjoin-pool test >>outX_poolSize10.txt
+mvn -B -Dbenchmark.poolSize=10 -Dbenchmark.executorServiceType=thread-pool test >>outX_poolSize10.txt
