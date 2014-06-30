401806b37d9ae7794179df6f3954abb03bb9db0c
Andriy Plokhotnyuk <a.plokhotnyuk@thenewmotion.com>
2014-07-01 01:20:19 +0300

    added UnboundedMailbox2 for Akka actors

--- src/test/scala/akka/dispatch/NonBlockingBoundedMailbox.scala
+++ src/test/scala/akka/dispatch/NonBlockingBoundedMailbox.scala
@@ -13,11 +13,11 @@ class NonBlockingBoundedMailbox(capacity: Int = Int.MaxValue) extends MailboxTyp
 }
 
 private class NBBQ(capacity: Int) extends AtomicInteger with MessageQueue with MultipleConsumerSemantics {
-  private val head = new AtomicReference(new NBBQNode)
+  private val head = new AtomicReference(new QNode)
   private val tail = new AtomicReference(head.get)
 
   override def enqueue(receiver: ActorRef, handle: Envelope): Unit = {
-    val n = new NBBQNode(handle)
+    val n = new QNode(handle)
     val h = head
     if (capacity > getAndIncrement()) h.getAndSet(n).set(n)
     else {
@@ -30,7 +30,7 @@ private class NBBQ(capacity: Int) extends AtomicInteger with MessageQueue with M
 
   override def numberOfMessages: Int = get
 
-  override def hasMessages: Boolean = get > 0
+  override def hasMessages: Boolean = 0 < get
 
   override def cleanUp(owner: ActorRef, deadLetters: MessageQueue): Unit = {
     var e = dequeue()
@@ -44,7 +44,7 @@ private class NBBQ(capacity: Int) extends AtomicInteger with MessageQueue with M
     a.asInstanceOf[InternalActorRef].provider.deadLetters.tell(DeadLetter(e.message, e.sender, a), e.sender)
 
   @annotation.tailrec
-  private def poll(t: AtomicReference[NBBQNode]): Envelope = {
+  private def poll(t: AtomicReference[QNode]): Envelope = {
     val tn = t.get
     val n = tn.get
     if (n ne null) {
@@ -58,4 +58,53 @@ private class NBBQ(capacity: Int) extends AtomicInteger with MessageQueue with M
   }
 }
 
-private class NBBQNode(var handle: Envelope = null) extends AtomicReference[NBBQNode]
+class UnboundedMailbox2 extends MailboxType with ProducesMessageQueue[MessageQueue] {
+  def this(settings: ActorSystem.Settings, config: Config) = this()
+
+  override def create(owner: Option[ActorRef], system: Option[ActorSystem]): MessageQueue = new UQ
+}
+
+private class UQ extends AtomicReference(new QNode) with MessageQueue with MultipleConsumerSemantics {
+  private val tail = new AtomicReference(get)
+
+  override def enqueue(receiver: ActorRef, handle: Envelope): Unit = {
+    val n = new QNode(handle)
+    getAndSet(n).set(n)
+  }
+
+  override def dequeue(): Envelope = poll(tail)
+
+  override def numberOfMessages: Int = count(tail.get, 0)
+
+  override def hasMessages: Boolean = null ne tail.get.get
+
+  override def cleanUp(owner: ActorRef, deadLetters: MessageQueue): Unit = {
+    var e = dequeue()
+    while (e ne null) {
+      deadLetters.enqueue(owner, e)
+      e = dequeue()
+    }
+  }
+
+  @annotation.tailrec
+  private def poll(t: AtomicReference[QNode]): Envelope = {
+    val tn = t.get
+    val n = tn.get
+    if (n ne null) {
+      if (t.compareAndSet(tn, n)) {
+        val e = n.handle
+        n.handle = null // to avoid possible memory leak when queue is empty
+        e
+      } else poll(t)
+    } else null
+  }
+
+  @annotation.tailrec
+  private def count(tn: AtomicReference[QNode], i: Int): Int = {
+    val n = tn.get
+    if (n eq null) i
+    else count(n, i + 1)
+  }
+}
+
+private class QNode(var handle: Envelope = null) extends AtomicReference[QNode]
---
+++ src/test/scala/com/github/plokhotnyuk/actors/AkkaBalanceableActorSpec.scala
@@ -0,0 +1,22 @@
+package com.github.plokhotnyuk.actors
+
+import com.typesafe.config.Config
+import com.typesafe.config.ConfigFactory._
+
+class AkkaBalanceableActorSpec extends AkkaActorSpec {
+  override def config: Config = load(parseString(
+    """
+      akka {
+        log-dead-letters = 0
+        log-dead-letters-during-shutdown = off
+        actor {
+          unstarted-push-timeout = 100s
+          benchmark-dispatcher {
+            executor = "com.github.plokhotnyuk.actors.CustomExecutorServiceConfigurator"
+            throughput = 1024
+            mailbox-type = "akka.dispatch.UnboundedMailbox2"
+          }
+        }
+      }
+    """))
+}
