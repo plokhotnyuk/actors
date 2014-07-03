b8613902e35a8db8417698778fcc08119bbf7a45
Andriy Plokhotnyuk <a.plokhotnyuk@thenewmotion.com>
2014-07-03 18:50:49 +0300

    try to minimize false sharing for non blocking bounded multi-consumer mailbox

--- src/test/scala/akka/dispatch/Mailboxes.scala
+++ src/test/scala/akka/dispatch/Mailboxes.scala
@@ -25,7 +25,7 @@ private class NBBQ(capacity: Int) extends AtomicReference(new QNodeWithCount) wi
 
   override def numberOfMessages: Int = get.count - tail.get.count
 
-  override def hasMessages: Boolean = get ne tail.get
+  override def hasMessages: Boolean = tail.get.get ne null
 
   override def cleanUp(owner: ActorRef, deadLetters: MessageQueue): Unit = {
     var e = dequeue()
