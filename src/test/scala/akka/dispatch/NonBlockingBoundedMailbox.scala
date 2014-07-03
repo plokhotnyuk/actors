e2c65a9ad1191d0152bfcd599a00043920b08af2
Andriy Plokhotnyuk <a.plokhotnyuk@thenewmotion.com>
2014-07-03 10:43:02 +0300

    rollback to version with counter for non blocking bounded mailbox

--- src/test/scala/akka/dispatch/NonBlockingBoundedMailbox.scala
+++ src/test/scala/akka/dispatch/NonBlockingBoundedMailbox.scala
@@ -1,8 +1,8 @@
 package akka.dispatch
 
-import akka.actor.{InternalActorRef, ActorRef, ActorSystem, DeadLetter}
+import java.util.concurrent.atomic.AtomicReference
 import com.typesafe.config.Config
-import java.util.concurrent.atomic.{AtomicInteger, AtomicReference}
+import akka.actor.{InternalActorRef, ActorRef, ActorSystem, DeadLetter}
 
 class NonBlockingBoundedMailbox(capacity: Int = Int.MaxValue) extends MailboxType with ProducesMessageQueue[MessageQueue] {
   if (capacity <= 0) throw new IllegalArgumentException("Mailbox capacity should be greater than 0")
@@ -12,24 +12,20 @@ class NonBlockingBoundedMailbox(capacity: Int = Int.MaxValue) extends MailboxTyp
   override def create(owner: Option[ActorRef], system: Option[ActorSystem]): MessageQueue = new NBBQ(capacity)
 }
 
-private final class NBBQ(capacity: Int) extends AtomicInteger with MessageQueue with MultipleConsumerSemantics {
-  private val head = new AtomicReference(new QNode)
-  private val tail = new AtomicReference(head.get)
+private class NBBQ(capacity: Int) extends AtomicReference(new QNodeWithCount) with MessageQueue with MultipleConsumerSemantics {
+  private val tail = new AtomicReference(get)
 
   override def enqueue(receiver: ActorRef, handle: Envelope): Unit =
-    if (capacity > getAndIncrement()) {
-      val n = new QNode(handle)
-      head.getAndSet(n).set(n)
-    } else {
-      getAndDecrement()
-      overflow(receiver, handle)
+    if (!offer(new QNodeWithCount(handle), tail.get.count)) {
+      receiver.asInstanceOf[InternalActorRef].provider.deadLetters
+        .tell(DeadLetter(handle.message, handle.sender, receiver), handle.sender)
     }
 
   override def dequeue(): Envelope = poll(tail)
 
-  override def numberOfMessages: Int = get
+  override def numberOfMessages: Int = get.count - tail.get.count
 
-  override def hasMessages: Boolean = 0 < get
+  override def hasMessages: Boolean = get ne tail.get
 
   override def cleanUp(owner: ActorRef, deadLetters: MessageQueue): Unit = {
     var e = dequeue()
@@ -39,16 +35,29 @@ private final class NBBQ(capacity: Int) extends AtomicInteger with MessageQueue 
     }
   }
 
-  private def overflow(a: ActorRef, e: Envelope): Unit =
-    a.asInstanceOf[InternalActorRef].provider.deadLetters.tell(DeadLetter(e.message, e.sender, a), e.sender)
+  @annotation.tailrec
+  private def offer(n: QNodeWithCount, tc: Int): Boolean = {
+    val h = get
+    val hc = h.count
+    if (hc - tc < capacity) {
+      n.count = hc + 1
+      if (compareAndSet(h, n)) {
+        h.set(n)
+        true
+      } else offer(n, tc)
+    } else {
+      val ntc = tail.get.count
+      if (tc == ntc) false
+      else offer(n, ntc)
+    }
+  }
 
   @annotation.tailrec
-  private def poll(t: AtomicReference[QNode]): Envelope = {
+  private def poll(t: AtomicReference[QNodeWithCount]): Envelope = {
     val tn = t.get
     val n = tn.get
     if (n ne null) {
       if (t.compareAndSet(tn, n)) {
-        getAndDecrement()
         val e = n.handle
         n.handle = null // to avoid possible memory leak when queue is empty
         e
@@ -57,13 +66,17 @@ private final class NBBQ(capacity: Int) extends AtomicInteger with MessageQueue 
   }
 }
 
+private class QNodeWithCount(var handle: Envelope = null) extends AtomicReference[QNodeWithCount] {
+  var count: Int = _
+}
+
 class UnboundedMailbox2 extends MailboxType with ProducesMessageQueue[MessageQueue] {
   def this(settings: ActorSystem.Settings, config: Config) = this()
 
   override def create(owner: Option[ActorRef], system: Option[ActorSystem]): MessageQueue = new UQ
 }
 
-private final class UQ extends AtomicReference(new QNode) with MessageQueue with MultipleConsumerSemantics {
+private class UQ extends AtomicReference(new QNode) with MessageQueue with MultipleConsumerSemantics {
   private val tail = new AtomicReference(get)
 
   override def enqueue(receiver: ActorRef, handle: Envelope): Unit = {
@@ -75,7 +88,7 @@ private final class UQ extends AtomicReference(new QNode) with MessageQueue with
 
   override def numberOfMessages: Int = count(tail.get, 0)
 
-  override def hasMessages: Boolean = null ne tail.get.get
+  override def hasMessages: Boolean = get ne tail.get
 
   override def cleanUp(owner: ActorRef, deadLetters: MessageQueue): Unit = {
     var e = dequeue()
