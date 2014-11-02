538ab4768b1e93489d24bcd62478acbcf2f757b7
Andriy Plokhotnyuk <a.plokhotnyuk@thenewmotion.com>
2014-11-02 18:09:05 +0200

    Removed catching unhandled errors

--- src/test/scala/scalaz/concurrent/ActorStrategy.scala
+++ src/test/scala/scalaz/concurrent/ActorStrategy.scala
@@ -4,67 +4,53 @@ import java.util.concurrent.ExecutorService
 
 object ActorStrategy {
   def apply(exec: ExecutorService): Strategy = exec match {
-    case p: scala.concurrent.forkjoin.ForkJoinPool =>
-      import scala.concurrent.forkjoin._
-      new Strategy {
-        def apply[A](a: => A): () => A = {
-          val t = new ForkJoinTask[Unit] {
-            def getRawResult(): Unit = ()
+    case p: scala.concurrent.forkjoin.ForkJoinPool => new Strategy {
 
-            def setRawResult(unit: Unit): Unit = ()
+      import scala.concurrent.forkjoin.ForkJoinTask
 
-            def exec(): Boolean = {
-              try a catch {
-                case ex: Throwable => onError(ex)
-              }
-              false
-            }
+      def apply[A](a: => A): () => A = {
+        val t = new ForkJoinTask[Unit] {
+          def getRawResult(): Unit = ()
+
+          def setRawResult(unit: Unit): Unit = ()
+
+          def exec(): Boolean = {
+            a
+            false
           }
-          if (ForkJoinTask.getPool eq p) t.fork()
-          else p.execute(t)
-          null
         }
+        if (ForkJoinTask.getPool eq p) t.fork()
+        else p.execute(t)
+        null
       }
-    case p: java.util.concurrent.ForkJoinPool =>
-      import java.util.concurrent._
-      new Strategy {
-        def apply[A](a: => A): () => A = {
-          val t = new ForkJoinTask[Unit] {
-            def getRawResult(): Unit = ()
-
-            def setRawResult(unit: Unit): Unit = ()
-
-            def exec(): Boolean = {
-              try a catch {
-                case ex: Throwable => onError(ex)
-              }
-              false
-            }
-          }
-          if (ForkJoinTask.getPool eq p) t.fork()
-          else p.execute(t)
-          null
-        }
-      }
-    case p =>
-      new Strategy {
-        def apply[A](a: => A): () => A = {
-          p.execute(new Runnable() {
-            def run(): Unit = try a catch {
-              case ex: Throwable => onError(ex)
-            }
-          })
-          null
-        }
-      }
-  }
-
-  private def onError(ex: Throwable): Unit =
-    if (ex.isInstanceOf[InterruptedException]) Thread.currentThread.interrupt()
-    else {
-      val ct = Thread.currentThread
-      val h = ct.getUncaughtExceptionHandler
-      if (h ne null) h.uncaughtException(ct, ex)
-      throw ex
     }
+    case p: java.util.concurrent.ForkJoinPool => new Strategy {
+
+      import java.util.concurrent.ForkJoinTask
+
+      def apply[A](a: => A): () => A = {
+        val t = new ForkJoinTask[Unit] {
+          def getRawResult(): Unit = ()
+
+          def setRawResult(unit: Unit): Unit = ()
+
+          def exec(): Boolean = {
+            a
+            false
+          }
+        }
+        if (ForkJoinTask.getPool eq p) t.fork()
+        else p.execute(t)
+        null
+      }
+    }
+    case p => new Strategy {
+      def apply[A](a: => A): () => A = {
+        p.execute(new Runnable() {
+          def run(): Unit = a
+        })
+        null
+      }
+    }
+  }
 }
