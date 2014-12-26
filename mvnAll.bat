602ad599e324affe1125d42001b8fe6ec5d59354
Andriy Plokhotnyuk <a.plokhotnyuk@thenewmotion.com>
2014-12-26 20:23:36 +0200

    Fix misprint in a benchmark param

--- mvnAll.bat
+++ mvnAll.bat
@@ -3,5 +3,5 @@ call mvn -B clean test-compile >outX.txt
 call mvn -B -Dbenchmark.executorServiceType=akka-forkjoin-pool test >>outX.txt
 call mvn -B -Dbenchmark.executorServiceType=scala-forkjoin-pool test >>outX.txt
 call mvn -B -Dbenchmark.executorServiceType=java-forkjoin-pool test >>outX.txt
-call mvn -B -Dbenchmark.executorServiceType=qbq-thread-pool test >>outX.txt
+call mvn -B -Dbenchmark.executorServiceType=abq-thread-pool test >>outX.txt
 call mvn -B -Dbenchmark.executorServiceType=lbq-thread-pool test >>outX.txt
