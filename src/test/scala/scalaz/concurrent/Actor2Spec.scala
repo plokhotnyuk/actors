af34db213d633ae12f007dd8cc3cf0be76cf4b5a
Andriy Plokhotnyuk <a.plokhotnyuk@thenewmotion.com>
2014-12-28 14:00:23 +0200

    Cleanup of test code

--- src/test/scala/scalaz/concurrent/Actor2Spec.scala
+++ src/test/scala/scalaz/concurrent/Actor2Spec.scala
@@ -3,6 +3,7 @@ package scalaz.concurrent
 import java.util.concurrent._
 import java.util.concurrent.atomic.AtomicInteger
 import org.specs2.mutable.Specification
+import scalaz.concurrent.Actor2._
 
 class Actor2Spec extends Specification {
   args(sequential = true)
@@ -11,104 +12,97 @@ class Actor2Spec extends Specification {
   val NumOfThreads = 4
 
   "actor with sequential actor strategy" should {
-    implicit val s: ActorStrategy = ActorStrategy.Sequential
+    implicit val s = ActorStrategy.Sequential
     unboundedActorTests(NumOfMessages)
     boundedActorTests(NumOfMessages)
   }
 
   "actor with actor strategy backed by Scala fork-join pool" should {
-    implicit val s: ActorStrategy = ActorStrategy.Executor(new scala.concurrent.forkjoin.ForkJoinPool())
+    implicit val s = ActorStrategy.Executor(new scala.concurrent.forkjoin.ForkJoinPool())
     unboundedActorTests(NumOfMessages)
     boundedActorTests(NumOfMessages)
   }
 
   "actor with actor strategy backed by Java fork-join pool" should {
-    implicit val s: ActorStrategy = ActorStrategy.Executor(new ForkJoinPool())
+    implicit val s = ActorStrategy.Executor(new ForkJoinPool())
     unboundedActorTests(NumOfMessages)
     boundedActorTests(NumOfMessages)
   }
 
   "actor with actor strategy backed by fixed thread pool" should {
-    implicit val s: ActorStrategy = ActorStrategy.Executor(Strategy.DefaultExecutorService)
+    implicit val s = ActorStrategy.Executor(Strategy.DefaultExecutorService)
     unboundedActorTests(NumOfMessages)
     boundedActorTests(NumOfMessages)
   }
 
   def unboundedActorTests(n: Int)(implicit s: ActorStrategy) = {
     "execute code async" in {
-      val latch = new CountDownLatch(1)
-      val actor = Actor2.unboundedActor[Int]((i: Int) => latch.countDown())
-      actor ! 1
-      assertCountDown(latch)
+      val l = new CountDownLatch(1)
+      unboundedActor[Int]((i: Int) => l.countDown()) ! 1
+      assertCountDown(l)
     }
 
     "catch code errors that can be handled" in {
-      val latch = new CountDownLatch(1)
-      val actor = Actor2.unboundedActor[Int]((i: Int) => 1 / 0, (ex: Throwable) => latch.countDown())
-      actor ! 1
-      assertCountDown(latch)
+      val l = new CountDownLatch(1)
+      unboundedActor[Int]((i: Int) => 1 / 0, (ex: Throwable) => l.countDown()) ! 1
+      assertCountDown(l)
     }
 
     "exchange messages with another actor without loss" in {
-      val latch = new CountDownLatch(n)
-      var actor1: Actor2[Int] = null
-      val actor2 = Actor2.unboundedActor[Int]((i: Int) => actor1 ! i - 1)
-      actor1 = Actor2.unboundedActor[Int] {
+      val l = new CountDownLatch(n)
+      var a1: Actor2[Int] = null
+      val a2 = unboundedActor[Int]((i: Int) => a1 ! i - 1)
+      a1 = unboundedActor[Int] {
         (i: Int) =>
-          if (i == latch.getCount) {
-            if (i != 0) actor2 ! i - 1
-            latch.countDown()
-            latch.countDown()
+          if (i == l.getCount) {
+            if (i != 0) a2 ! i - 1
+            l.countDown()
+            l.countDown()
           }
       }
-      actor1 ! n
-      assertCountDown(latch)
+      a1 ! n
+      assertCountDown(l)
     }
 
     "create child actor and send messages to it recursively" in {
-      val latch = new CountDownLatch(1)
+      val l = new CountDownLatch(1)
 
-      def actor: Actor2[Int] = Actor2.unboundedActor[Int] {
-        (i: Int) =>
-          if (i > 0) actor ! i - 1
-          else latch.countDown()
-      }
+      def a: Actor2[Int] = unboundedActor[Int]((i: Int) => if (i > 0) a ! i - 1 else l.countDown())
 
-      actor ! (if (s eq  ActorStrategy.Sequential) 300 else n)
-      assertCountDown(latch)
+      a ! (if (s eq  ActorStrategy.Sequential) 300 else n)
+      assertCountDown(l)
     }
 
     "handle messages in order of sending by each thread" in {
       val nRounded = (n / NumOfThreads) * NumOfThreads
-      val latch = new CountDownLatch(nRounded)
-      val actor = countingDownUnboundedActor(latch)
+      val l = new CountDownLatch(nRounded)
+      val a = countingDownUnboundedActor(l)
       for (j <- 1 to NumOfThreads) fork {
         for (i <- 1 to nRounded / NumOfThreads) {
-          actor ! j -> i
+          a ! j -> i
         }
       }
-      assertCountDown(latch)
+      assertCountDown(l)
     }
 
     "doesn't handle messages in simultaneous threads" in {
       val nRounded = (n / NumOfThreads) * NumOfThreads
-      val latch = new CountDownLatch(1)
-      val actor = Actor2.boundedActor[Int](Int.MaxValue, {
+      val l = new CountDownLatch(1)
+      val a = boundedActor[Int](Int.MaxValue, {
         var sum = 0L
         val expectedSum = nRounded * (nRounded + 1L) / 2
-
         (i: Int) =>
           sum += i
-          if (sum == expectedSum) latch.countDown()
+          if (sum == expectedSum) l.countDown()
       })
       val numOfMessagesPerThread = nRounded / NumOfThreads
       for (j <- 1 to NumOfThreads) fork {
         val off = (j - 1) * numOfMessagesPerThread
         for (i <- 1 to numOfMessagesPerThread) {
-          actor ! i + off
+          a ! i + off
         }
       }
-      assertCountDown(latch)
+      assertCountDown(l)
     }
 
     "redirect unhandled errors to uncaught exception handler of thread" in {
@@ -118,7 +112,7 @@ class Actor2Spec extends Specification {
         System.setErr(new java.io.PrintStream(new java.io.OutputStream {
           override def write(b: Int): Unit = l.countDown()
         }))
-        Actor2.unboundedActor((_: Int) => 1 / 0) ! 1
+        unboundedActor((_: Int) => 1 / 0) ! 1
         assertCountDown(l)
       } catch {
         case e: ArithmeticException if e.getMessage == "/ by zero" =>
@@ -130,46 +124,40 @@ class Actor2Spec extends Specification {
 
   def boundedActorTests(n: Int)(implicit s: ActorStrategy) = {
     "execute code async" in {
-      val latch = new CountDownLatch(1)
-      val actor = Actor2.boundedActor[Int](Int.MaxValue, (i: Int) => latch.countDown())
-      actor ! 1
-      assertCountDown(latch)
+      val l = new CountDownLatch(1)
+      boundedActor[Int](Int.MaxValue, (i: Int) => l.countDown()) ! 1
+      assertCountDown(l)
     }
 
     "catch code errors that can be handled" in {
-      val latch = new CountDownLatch(1)
-      val actor = Actor2.boundedActor[Int](Int.MaxValue, (i: Int) => 1 / 0, (ex: Throwable) => latch.countDown())
-      actor ! 1
-      assertCountDown(latch)
+      val l = new CountDownLatch(1)
+      boundedActor[Int](Int.MaxValue, (i: Int) => 1 / 0, (ex: Throwable) => l.countDown()) ! 1
+      assertCountDown(l)
     }
 
     "exchange messages with another actor without loss" in {
-      val latch = new CountDownLatch(n)
-      var actor1: Actor2[Int] = null
-      val actor2 = Actor2.boundedActor[Int](Int.MaxValue, (i: Int) => actor1 ! i - 1)
-      actor1 = Actor2.boundedActor[Int](Int.MaxValue,
+      val l = new CountDownLatch(n)
+      var a1: Actor2[Int] = null
+      val a2 = boundedActor[Int](Int.MaxValue, (i: Int) => a1 ! i - 1)
+      a1 = boundedActor[Int](Int.MaxValue,
         (i: Int) =>
-          if (i == latch.getCount) {
-            if (i != 0) actor2 ! i - 1
-            latch.countDown()
-            latch.countDown()
+          if (i == l.getCount) {
+            if (i != 0) a2 ! i - 1
+            l.countDown()
+            l.countDown()
           }
       )
-      actor1 ! n
-      assertCountDown(latch)
+      a1 ! n
+      assertCountDown(l)
     }
 
     "create child actor and send messages to it recursively" in {
-      val latch = new CountDownLatch(1)
+      val l = new CountDownLatch(1)
 
-      def actor: Actor2[Int] = Actor2.boundedActor[Int](Int.MaxValue,
-        (i: Int) =>
-          if (i > 0) actor ! i - 1
-          else latch.countDown()
-      )
+      def a: Actor2[Int] = boundedActor[Int](Int.MaxValue, (i: Int) => if (i > 0) a ! i - 1 else l.countDown())
 
-      actor ! (if (s eq  ActorStrategy.Sequential) 300 else n)
-      assertCountDown(latch)
+      a ! (if (s eq  ActorStrategy.Sequential) 300 else n)
+      assertCountDown(l)
     }
 
     "handle messages in order of sending by each thread" in {
@@ -186,24 +174,22 @@ class Actor2Spec extends Specification {
 
     "doesn't handle messages in simultaneous thread" in {
       val nRounded = (n / NumOfThreads) * NumOfThreads
-      val latch = new CountDownLatch(1)
-      val actor = Actor2.boundedActor[Int](Int.MaxValue, {
+      val l = new CountDownLatch(1)
+      val a = boundedActor[Int](Int.MaxValue, {
         var sum = 0L
         val expectedSum = nRounded * (nRounded + 1L) / 2
-
         (i: Int) =>
           sum += i
-          if (sum == expectedSum) latch.countDown()
-
+          if (sum == expectedSum) l.countDown()
       })
       val numOfMessagesPerThread = nRounded / NumOfThreads
       for (j <- 1 to NumOfThreads) fork {
         val off = (j - 1) * numOfMessagesPerThread
         for (i <- 1 to numOfMessagesPerThread) {
-          actor ! i + off
+          a ! i + off
         }
       }
-      assertCountDown(latch)
+      assertCountDown(l)
     }
 
     "redirect unhandled errors to uncaught exception handler of thread" in {
@@ -213,7 +199,7 @@ class Actor2Spec extends Specification {
         System.setErr(new java.io.PrintStream(new java.io.OutputStream {
           override def write(b: Int): Unit = l.countDown()
         }))
-        Actor2.boundedActor(Int.MaxValue, (_: Int) => 1 / 0) ! 1
+        boundedActor(Int.MaxValue, (_: Int) => 1 / 0) ! 1
         assertCountDown(l)
       } catch {
         case e: ArithmeticException if e.getMessage == "/ by zero" =>
@@ -223,34 +209,32 @@ class Actor2Spec extends Specification {
     }
 
     "be bounded by positive number" in {
-      Actor2.boundedActor(0, (_: Int) => ()) must throwA[IllegalArgumentException]
+      boundedActor(0, (_: Int) => ()) must throwA[IllegalArgumentException]
     }
 
     "handle overflow" in {
       val i = new AtomicInteger
-      val a = Actor2.boundedActor(1, (_: Int) => (), onOverflow = (_: Int) => i.incrementAndGet())
+      val a = boundedActor(1, (_: Int) => (), onOverflow = (_: Int) => i.incrementAndGet())
       (1 to NumOfMessages).foreach(a ! _)
       if (s eq ActorStrategy.Sequential) i.get must_== 0 // a sequential strategy never overflow
       else i.get must be greaterThan 0
     }
   }
 
-  def countingDownUnboundedActor(latch: CountDownLatch)(implicit s: ActorStrategy): Actor2[(Int, Int)] =
-    Actor2.unboundedActor[(Int, Int)] {
+  private def countingDownUnboundedActor(l: CountDownLatch)(implicit s: ActorStrategy): Actor2[(Int, Int)] =
+    unboundedActor[(Int, Int)] {
       val ms = collection.mutable.Map[Int, Int]()
-
       (m: (Int, Int)) =>
         val (j, i) = m
         if (ms.getOrElse(j, 0) + 1 == i) {
           ms.put(j, i)
-          latch.countDown()
+          l.countDown()
         }
     }
 
   private def countingDownBoundedActor(l: CountDownLatch)(implicit s: ActorStrategy): Actor2[(Int, Int)] =
-    Actor2.boundedActor(Int.MaxValue, {
+    boundedActor(Int.MaxValue, {
       val ms = collection.mutable.Map[Int, Int]()
-
       (m: (Int, Int)) =>
         val (j, i) = m
         if (ms.getOrElse(j, 0) + 1 == i) {
@@ -259,8 +243,8 @@ class Actor2Spec extends Specification {
         }
     })
 
-  private def assertCountDown(latch: CountDownLatch, timeout: Long = 1000): Boolean =
-    latch.await(timeout, TimeUnit.MILLISECONDS) must_== true
+  private def assertCountDown(l: CountDownLatch, timeout: Long = 1000): Boolean =
+    l.await(timeout, TimeUnit.MILLISECONDS) must_== true
 
   private def fork(f: => Unit): Unit = new Thread {
     override def run(): Unit = f
