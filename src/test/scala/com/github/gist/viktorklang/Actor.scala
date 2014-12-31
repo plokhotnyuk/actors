1d672f328460d496c5bd3b6572ba548beb66d69b
Andriy Plokhotnyuk <a.plokhotnyuk@thenewmotion.com>
2014-12-31 17:59:57 +0200

    Clean up code

--- src/test/scala/com/github/gist/viktorklang/Actor.scala
+++ src/test/scala/com/github/gist/viktorklang/Actor.scala
@@ -25,7 +25,7 @@ object Actor {
   case class Become(like: Behavior) extends Effect { def apply(old: Behavior): Behavior = like }
   final val Die = Become(msg => sys.error("Dropping of message due to severe case of death: " + msg))
   trait Address { def !(msg: Any): Unit } // The notion of an Address to where you can post messages to
-  def apply(initial: Behavior, batch: Int = 5)(implicit e: Executor): Address = // Seeded by the self-reference that yields the initial behavior
+  def apply(initial: Behavior, batch: Int = 5)(implicit e: Executor): Address = // Seeded by the initial behavior
     new AtomicReference[Node] with Address { // Memory visibility of behavior is guarded by volatile piggybacking
       private var b: Behavior = initial // Rebindable top of the mailbox
       def !(msg: Any): Unit = { val n = Node(msg); val h = getAndSet(n); if (h ne null) h.lazySet(n) else schedule(n) } // Enqueue the message onto the mailbox and try to schedule for execution
