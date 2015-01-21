1c803bbd5e120aa79a952490cb128cb2ba765a0d
Andriy Plokhotnyuk <a.plokhotnyuk@thenewmotion.com>
2015-01-21 14:39:44 +0200

    Simplify code of the Minimalist Actor to be easier portable to Java

--- src/test/scala/com/github/gist/viktorklang/Actor.scala
+++ src/test/scala/com/github/gist/viktorklang/Actor.scala
@@ -29,7 +29,7 @@ object Actor {
   def apply(initial: Address => Behavior, batch: Int = 5)(implicit e: Executor): Address = // Seeded by the self-reference that yields the initial behavior
     new AtomicReference[AnyRef]((self: Any) => Become(initial(self.asInstanceOf[Address]))) with Address { // Memory visibility of behavior is guarded by volatile piggybacking & executor
       this ! this // Make the actor self aware by seeding its address to the initial behavior
-      def !(msg: Any): Unit = { val n = new Node(msg); getAndSet(n) match { case h: Node => h.lazySet(n); case b: Behavior @unchecked => async(b, n, true) } } // Enqueue the message onto the mailbox or schedule for execution
+      def !(msg: Any): Unit = { val n = new Node(msg); getAndSet(n) match { case h: Node => h.lazySet(n); case b => async(b.asInstanceOf[Behavior], n, true) } } // Enqueue the message onto the mailbox or schedule for execution
       private def act(b: Behavior, n: Node): Unit = { var b1 = b; var n1, n2 = n; var i = batch; try do { n1 = n2; b1 = b1(n1.msg)(b1); n2 = n1.get; i -= 1 } while ((n2 ne null) && i != 0) finally async(b1, n1, false) } // Reduce messages to behaviour in batch loop
       private def actOrOff(b: Behavior, n: Node, x: Boolean): Unit = if (x) act(b, n) else if ((n ne get) || !compareAndSet(n, b)) act(b, n.next) // Act or switch ourselves off
       private def async(b: Behavior, n: Node, x: Boolean): Unit = e match { // Schedule for execution
